4c42d9fc8bd108f76b436b94492daee4bf4e0ec4
tkgandhi <109563744+tkgandhi@users.noreply.github.com>
2023-08-03 15:11:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 song_1 = "";
 song_2 = "";
-leftwristX = 0;
-leftwristY = 0;
-rightwristX = 0;
-rightwristY = 0;
+leftWristX=0;
+leftWristY=0;
+
+rightWristX=0;
+rightWristY=0;
+
+scoreRightWrist=0;
+scoreLeftWrist=0;
+
+song1_status="";
+song2_status="";
+
+function preload()
+{
+    song_1 = loadSound("music.mp3");
+    song_2 = loadSound("music2.mp3");
+}
 
 function setup() 
 {
@@ -17,34 +30,57 @@ function setup()
     poseNet.on("pose", gotPoses);
 }
 
-function modelLoaded() 
+function gotPoses(results)
 {
-    console.log("PoseNet is Initialized!");
-}
+    if(results.length > 0)
+    {
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
 
-function draw() 
-{
-    image(video, 0, 0, 600, 500);
-}
+        console.log("scoreLeftWrist = " + scoreLeftWrist + "scoreRightWrist = " + scoreRightWrist);
 
-function preload() 
-{
-    song_1 = loadSound("music.mp3");
-    song_2 = loadSound("music2.mp3");
-}
-
-function gotPoses(results) 
-{
-    if (results.length > 0) {
         console.log(results);
 
-        leftwristX = results[0].pose.leftWrist.x;
-        leftwristY = results[0].pose.leftWrist.y;
-        console.log("leftwristX = " + leftwristX + "leftwristY" + leftwristY);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
 
-        rightwristX = results[0].pose.rightWrist.x;
-        rightwristY = results[0].pose.rightWrist.y;
-        console.log("rightwristX = " + rightwristX + "rightwristY" + rightwristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
     }
+}
 
+function modelLoaded() 
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    song2_status = song_2.isPlaying();
+    song1_status = song_1.isPlaying();
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        song_2.stop();
+
+        if(song1_status == false){
+            song_1.play();
+            document.getElementById("song").innerHTML="Playing - Harry Potter Theme Song";
+        }
+    }
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
